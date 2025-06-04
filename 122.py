4ce61eb7443a261bf2dcd8f556d8c2a913813fad
debug-loop <glucas9594@outlook.com>
2025-06-04 18:46:42 -0500

    122 solution

---
+++ 122.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        i = 0
+        lo = prices[0]
+        hi = prices[0]
+        profit = 0
+        n = len(prices)
+
+        while i < n-1:
+            while i < n-1 and prices[i] >= prices[i+1]:
+                i += 1
+            lo = prices[i]
+
+            while i < n-1 and prices[i] <= prices[i+1]:
+                i += 1
+            hi = prices[i]
+            profit += hi - lo
+        
+        return profit
