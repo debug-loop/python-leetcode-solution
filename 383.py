63258a98e3b12d8de08aed6a3a9eb472435bb818
debug-loop <glucas9594@outlook.com>
2025-06-15 06:53:12 -0500

    383 solution

---
+++ 383.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+
+        counter = {}
+        for c in magazine:
+            if c in counter:
+                counter[c] += 1
+            else:
+                counter[c] = 1
+        
+        for c in ransomNote:
+            if c not in counter:
+                return False
+            elif counter[c] == 1:
+                del counter[c]
+            else:
+                counter[c] -= 1
+        
+        return True
