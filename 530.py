84c78cb1f62b2cb810d1a6450aee6c165c511006
debug-loop <glucas9594@outlook.com>
2025-06-22 09:28:52 -0500

    530 solution

---
+++ 530.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def getMinimumDifference(self, root):
+        min_distance = [float('inf')]
+        prev = [None]
+
+        def dfs(node):
+            if node is None:
+                return
+            
+            dfs(node.left)
+
+            if prev[0] is not None:
+                min_distance[0] = min(min_distance[0], node.val - prev[0])
+            
+            prev[0] = node.val
+
+            dfs(node.right)
+        
+        dfs(root)
+        return min_distance[0]
