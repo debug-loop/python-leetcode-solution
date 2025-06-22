2b8689c263a4b9f96682ee17ab564d70a53cf685
debug-loop <glucas9594@outlook.com>
2025-06-22 16:04:24 -0500

    46 solution

---
+++ 46.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def permute(self, nums):
+        n = len(nums)
+        ans, sol = [], []
+
+        def backtrack():
+            if len(sol) == n:
+                ans.append(sol[:])
+                return
+            
+            for x in nums:
+                if x not in sol:
+                    sol.append(x)
+                    backtrack()
+                    sol.pop()
+        
+        backtrack()
+        return ans
